9a58a353e4136f83aefee90c4f4daff749f72b3d
Dino-svg <angel.cortes8356@alumnos.udg.mx>
2025-05-04 12:11:59 -0600

    Update Banco_registros.v

--- Banco_registros.v
+++ Banco_registros.v
@@ -1,24 +1,27 @@
 module BR(
-	input  wire[4:0]adrsReadA,
-	input  wire[4:0]adrsReadB,
-	input  wire[4:0]adrsWrite,
-	input  wire RegEn,
-	input  wire[31:0] write,
-	output reg [31:0] readA,
-	output reg [31:0] readB
+    input clk,                 
+    input [4:0] adrsReadA,
+    input [4:0] adrsReadB,
+    input [4:0] adrsWrite,
+    input RegEn,
+    input [31:0] write,
+    output reg [31:0] readA,
+    output reg [31:0] readB
 );
 
-reg [31:0]BR_in[0:31];
+reg [31:0] BR_in[0:31];
 
-always@(*)
-	begin
-	readA = BR_in[adrsReadA];
-	readB = BR_in[adrsReadB];
-	
-	if(RegEn)
-	begin
-		BR_in[adrsWrite] = write;
-	end
-	
+// Lectura asíncrona
+always @(*) begin
+    readA = BR_in[adrsReadA];
+    readB = BR_in[adrsReadB];
 end
+
+// Escritura síncrona
+always @(posedge clk) begin
+    if (RegEn) begin
+        BR_in[adrsWrite] <= write;
+    end
+end
+
 endmodule
