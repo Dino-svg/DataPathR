4b168e77b764b15aa4fc1b7e5902fe57c0ab4eef
Dino-svg <angel.cortes8356@alumnos.udg.mx>
2025-05-04 19:46:41 -0600

    Update MIPS_Top.v

--- MIPS_Top.v
+++ MIPS_Top.v
@@ -39,9 +39,9 @@ end
 initial begin
     #60;  // Esperar ejecución de Sub $20 $15 $9 ($20 = 999 - 100 = 899)
     if (resultado == 32'd899) begin
-        $display("✅ $20 = %d (Correcto)", resultado);
+        $display(" $20 = %d (Correcto)", resultado);
     end else begin
-        $display("❌ $20 = %d (Debería ser 899)", resultado);
+        $display(" $20 = %d (Debería ser 899)", resultado);
     end
     #40;  // Esperar Add $15 $5 $15 ($15 = 20 + 999 = 1019)
     $display("$15 = %d", dut.DataPathTipoR.inst_BR.BR_in[15]);
