c843fa0499ad6283220efbf84b06a03ea7fe5412
Dino-svg <angel.cortes8356@alumnos.udg.mx>
2025-05-04 19:45:54 -0600

    Update Unidad_control.v

--- Unidad_control.v
+++ Unidad_control.v
@@ -3,10 +3,6 @@ module Unidad_control(
     output reg [5:0] salida_control
 );
 
-// Bit 5: RegWrite (1 para Tipo R)
-// Bits 4-2: ALUop (010 para Tipo R)
-// Bit 1: MemToWrite (0 para Tipo R)
-// Bit 0: MemToReg (1 para Tipo R, resultado de ALU)
 always @(*) begin
     case (OPcode)
         6'b000000: salida_control = 6'b1_010_0_1;  // Instrucciones Tipo R
